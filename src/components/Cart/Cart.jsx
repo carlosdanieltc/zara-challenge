a76884e6c2984802dba092bd1a9c6ceb482aa7e7
Carlos <carlosdanieltc@gmail.com>
2025-02-19 20:33:20 +0100

    Avance en Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,9 +3,10 @@ import React, { useEffect, useState } from 'react';
 import styles from './Cart.module.css'
 import { useCart } from '../../context/CartContext'
 import { Link } from 'react-router-dom';
+import PhoneCardCart from '../PhoneCard/PhoneCardCart'
 
 export const Cart = () => {
-    const { getItemCount, getTotal } = useCart();
+    const { cartItems, getItemCount, getTotal } = useCart();
 
     useEffect(() => {
         getItemCount();
@@ -15,7 +16,11 @@ export const Cart = () => {
         <div className={styles.cartContainer}>
             <div className={styles.cartItemsContainer}>
                 <h2 className={styles.cartItems}>CART ({getItemCount()})</h2>
-                <div className={styles.phoneCards}></div>
+                <div className={styles.phoneCards}>
+                    {cartItems.map((phone, index) => (
+                        <PhoneCardCart></PhoneCardCart>
+                    ))}
+                </div>
             </div>
             <div className={styles.cartOptions}>
                 <Link to="/">
---
+++ src/components/PhoneCard/PhoneCardCart.jsx
@@ -0,0 +1,16 @@
+
+import React, { useEffect, useState } from 'react';
+import styles from './PhoneCardCart.module.css'
+import { useCart } from '../../context/CartContext'
+import { Link } from 'react-router-dom';
+
+export const CardCart = () => {
+    
+    return (
+       <div className={styles.card}>
+        
+       </div>
+    );
+};
+
+export default CardCart;
